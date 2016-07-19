f16b1705adad97af1aafdcafb4f095be993fd070
himanshu0503 <hemanshu0503@gmail.com>
2016-07-19 17:08:57 +0530

    Updating counter from 159 to 160

--- updateCounter.sh
+++ updateCounter.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+pushd /home/himanshu/Projects/sample-runsh
 initialCounter=`cat shippable.triggers.yml | grep counter | awk {'print $2'}`
 incrementedCounter=$((initialCounter+1))
 sed "s~$initialCounter~$incrementedCounter~g" shippable.triggers.yml > temp
@@ -5,3 +7,4 @@ mv temp shippable.triggers.yml
 git add --all
 git commit -m "Updating counter from $initialCounter to $incrementedCounter"
 git push origin master
+popd
